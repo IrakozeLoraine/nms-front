bdc5f1fa1a461446463404cd027382ee8bf6c3bc
AngeNicole <angenicolemukundwa22@gmail.com>
2022-02-24 22:28:54 +0200

    fix: bar bg

--- src/components/molecule/PopupMolecule.tsx
+++ src/components/molecule/PopupMolecule.tsx
@@ -1,5 +1,21 @@
+import Bar from "../atoms/Bar";
+import Delete from '../../public/icons/ellipse.svg'
+import Popup from "reactjs-popup";
+
+type popupType = {
+    title: string
+    icon: object
+    swap?:boolean
+}
+
 export default function PopupMolecule() {
+    const icon = [
+        {
+            title: "delete",
+        },
+        
+    ]
     return (
-        <div></div>
+        <Popup children={undefined}></Popup>
     )
 }
