ea9bf46a3537b2d71f59f9246da6886bceb5aca1
AngeNicole <angenicolemukundwa22@gmail.com>
2022-03-14 21:03:09 +0200

    Fix: syntax

--- src/components/atoms/Bar.tsx
+++ src/components/atoms/Bar.tsx
@@ -1,49 +1,6 @@
-<<<<<<< HEAD
-import { BarType } from "../../types";
-import Download from '../../public/icons/download.svg'
-import { useState } from "react";
-
-
-export default function Bar(props: BarType) {
-    const [toggle, handleToggle] = useState<boolean>(false);
-    return (
-        <div className={`rounded-lg cursor-pointer mt-4 p-3  ${props.hasColor ? 'bg-gray-100' : 'bg-green-50'} `}
-            onClick={
-                () => {
-               props.subnotes.length !== 0 ? handleToggle(!toggle) : alert("This is a subnote")
-            }
-        }
-        >
-
-            <div className="flex justify-between items-center">
-                <div className="txt-primary">
-                    {props.title}
-                </div>
-                <div className="mr-4" onClick={
-                    () => {
-                        
-                    }
-                }>
-                    <img src={Download} alt="" className=""/>
-                </div>
-            </div>
-            {/* Subnotes */}
-            {/* {
-                toggle && <div className="border ">
-                    {
-                        props.subnotes.map((item: string, i) => {
-                            return (<Bar key={i} title={item} subnotes={[]} hasColor={props.hasColor} />)
-                        })
-                    }
-                </div>
-            } */}
-            </div>
-    )
-}
-=======
-import { BarType } from '../../types';
-import Delete from '../../public/icons/ellipse.svg';
-import { useState } from 'react';
+import { BarType } from '../../types'
+import Delete from '../../public/icons/ellipse.svg'
+import { useState } from 'react'
 
 export default function Bar({
   hasColor,
@@ -82,4 +39,3 @@ export default function Bar({
     </div>
   );
 }
->>>>>>> 8f991bc1543959ca4ad6129c9c62c37ff896610d
